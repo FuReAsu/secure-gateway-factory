785157d41385e9701f8c68077b4afbcd44720e8f
Hein Htet Zaw <maintain1346@gmail.com>
2026-02-28 15:48:26 +0630

    updated install-outline script

--- scripts/install-outline.sh
+++ scripts/install-outline.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
-set -euo pipefail
+
+mkdir -p $HOME/outline
+LOG_FILE="$HOME/outline/outline.log"
+INSTALL_FILE="$HOME/outline/install_server.sh"
 
 echo "==> [CONFIG] Checking required variables..."
 : "${API_PORT:?Error: API_PORT environment variable is not set}"
 : "${KEYS_PORT:?Error: KEYS_PORT environment variable is not set}"
 
 echo "==> [DOWNLOAD] Fetching Outline installation script..."
-if ! curl -sSL https://raw.githubusercontent.com/Jigsaw-Code/outline-apps/master/server_manager/install_scripts/install_server.sh -o install_server.sh; then
+if ! curl -sSL https://raw.githubusercontent.com/Jigsaw-Code/outline-apps/master/server_manager/install_scripts/install_server.sh -o $INSTALL_FILE; then
     echo "Falled to download installation script."
     exit 1
 fi
 
 echo "==> [INSTALL] Running Outline installer (this may take a minute)..."
 
-INSTALL_LOG=$(yes | sudo bash install_server.sh --api-port "$API_PORT" --keys-port "$KEYS_PORT" | tee /dev/stderr)
+yes | sudo bash $INSTALL_FILE --api-port "$API_PORT" --keys-port "$KEYS_PORT" > $LOG_FILE
 
 echo "==> [EXTRACT] Parsing API configuration..."
-RAW_CONFIG=$(echo "$INSTALL_LOG" | grep "apiUrl" || true)
+RAW_CONFIG=$(cat "$LOG_FILE" | grep "apiUrl" || true)
 
 if [[ -n "$RAW_CONFIG" ]]; then
     echo "$RAW_CONFIG" | grep -o '{.*}' > outline-api.json
