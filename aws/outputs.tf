f3d8add6397c6f8310368b82e5e9ea5d286fd4ad
Hein Htet Zaw <maintain1346@gmail.com>
2026-03-03 23:20:25 +0630

    removed keypair_name from outputs

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -2,10 +2,6 @@ output "instance_ip" {
   value = aws_instance.vpn-factory-server.public_ip
 }
 
-output "ssh_keypair_name" {
-  value = var.keypair_name
-}
-
 output "randomized_ports" {
   value = [
     random_shuffle.vpn_ports.result[0],
