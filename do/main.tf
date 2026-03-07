b2c5e2c84550c1a5f22b6fdf73d08be755c0e7bb
Hein Htet Zaw <maintain1346@gmail.com>
2026-03-07 13:10:35 +0630

    updated instance resource name and added timeout

--- do/main.tf
+++ do/main.tf
@@ -32,10 +32,14 @@ resource "digitalocean_ssh_key" "vpn-factory-key" {
   public_key = file("../ssh-keys/vpn-factory-key.pub")
 }
 
-resource "digitalocean_droplet" "vpn-factory-key" {
+resource "digitalocean_droplet" "vpn-factory-server" {
   image = "ubuntu-24-04-x64"
   name = "cloud-vpn-server"
   region = var.region
   size = var.instance_type
   ssh_keys = [digitalocean_ssh_key.vpn-factory-key.fingerprint]
+
+  timeouts {
+    delete = "5m"
+  }
 }
